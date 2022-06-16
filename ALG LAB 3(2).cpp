e523c851bd4b525ffe3d5f5a3a159f4553f59ee8
Ittero <99478086+Ittero@users.noreply.github.com>
2022-06-16 16:39:36 +0300

    Add files via upload

---
+++ ALG LAB 3(2).cpp
@@ -0,0 +1,41 @@
+﻿#include"CHER.h"
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cher W;
+	int f;
+	srand(time(0));
+	W.mass();
+	while (true)
+	{
+		W.empty();
+		cout << "1) -  MAKENULL" << endl
+			<< "2) - FRONT" << endl
+			<< "3) - ENQUEUE" << endl
+			<< "4) - DEQUEUE" << endl
+			<< "5) - PRINT" << endl
+			<< "Ваш вибір -  ";
+		cin >> f;
+		switch (f)
+		{
+		case 1:
+			W.MAKENULL();
+			break;
+		case 2:
+			W.FRONT();
+			break;
+		case 3:
+			W.ENQUEUE();
+			break;
+		case 4:
+			W.DEQUEUE();
+			break;
+		case 0:
+			W.PRINT();
+			break;
+		default:
+			cout << "Невірні дані!" << endl;
+		}
+	}
+}
---
+++ ALG LAB 3.cpp
@@ -0,0 +1,42 @@
+﻿#include "Stack.h"
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(0));
+	stack stc;
+	int q;
+	stc.fill();
+	while (true)
+	{
+		stc.empty();
+		cout << "1) - POP " << endl
+			<< "2) - TOP" << endl
+			<< "3) - PUSH " << endl
+			<< "4) - PRINT" << endl
+			<< "5) - MAKENULL" << endl
+			<< "Виберіть дію - ";
+		cin >> q;
+		switch (q)
+		{
+		case 1:
+			stc.POP();
+			break;
+		case 2:
+			stc.TOP();
+			break;
+		case 3:
+			stc.PUSH();
+			break;
+		case 4:
+			stc.PRINT();
+			break;
+		case 5:
+			stc.MAKENULL();
+			break;
+		default:
+			cout << "Ви ввели невірні дані!" << endl;
+			break;
+		}
+	}
+}
